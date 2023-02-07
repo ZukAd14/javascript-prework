eacbf843ef230917a5412af1f65652c730a25a53
Adam <adam.zukowski471@gmail.com>
2023-02-08 00:28:22 +0100

    Add const & unnamed functions

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
+{
 function playGame(playerInput){
     clearMessages();
 
-    function getMoveName(argMoveId){
+    const getMoveName = function(argMoveId){
         if(argMoveId == 1){
         return 'kamień';
         } else if(argMoveId == 2){
             return 'papier';
         }else if(argMoveId == 3){
             return 'nożyce'
+            
         }
-  
+        
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
+        
     }
-  
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
 
     console.log('Wylosowana liczba to: ' + randomNumber);
-
-    let computerMove = getMoveName(randomNumber);
+    
+    const computerMove = getMoveName(randomNumber);
 
     printMessage('Mój ruch to: ' + computerMove);
-
+    
     console.log('Gracz wpisał: ' + playerInput);
 
-    let playerMove = getMoveName(playerInput);
+    const playerMove = getMoveName(playerInput);
 
     printMessage('Twój ruch to: ' + playerMove);
 
-    function displayResult(argComputerMove, argPlayerMove){
+    const displayResult = function(argComputerMove, argPlayerMove){
         printMessage('Zagrałem ' + argComputerMove + ' a Ty ' + argPlayerMove)
 
         if(argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
@@ -65,3 +69,4 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);
 });
+}
